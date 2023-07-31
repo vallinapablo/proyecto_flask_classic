315c4451b5b4ef99b31f1bca1b19abead97310b4
Pablo Vallina <pablo@MacBook-Pro-de-Pablo.local>
2023-07-30 21:16:13 -0300

    Base html

--- my_crypto/routes.py
+++ my_crypto/routes.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route("/")
 def index():
     return render_template("inicio.html")
+
+@app.route("/purchase", methods=["GET"])
+def purchase():
+    return render_template("compra.html")
+
+@app.route("/status", methods=["GET"])
+def status():
+    return render_template("status.html")
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Aplicacion web: Registro de movimientos de criptomonedas
+
+Se trata de realizar un registro de inversiones y compra/venta de criptomonedas.
